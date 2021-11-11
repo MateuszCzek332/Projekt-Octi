d73c1de302b42dadf7042ab60bbdcfd28b2f635f
Mateusz Czekaj <mateusz.czek332@gmail.com>
2021-11-11 16:50:03 +0100

    Poprawki + naprawa buga z kamien papier nozyce

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 let btn = document.querySelector("#submit")
 let inp1 = document.querySelector("#input1")
 let ans = document.querySelector("#answer")
-let data = new Date()
 let g = false        
 let week = ["Niedziela ","Poniedziałek ","Wtorek ","Środa ","Czwartek ","Piątek ","Sobota ",]
-//tablice z keywordami i odpowiedziami
 let game_q = ["gra","kamien papier nozyce", "kamien, papier, nozyce", "kamień, papier, nożyce"]
+let math_q = ["+", "-", "*", "/", "%", "sqrt"]
+let date = ["data", "ktory dzis jest", "jaki mamy dzien", "co dzis jest"]
+let time = ["godzina", "ktora jest godzina", "jaki jest czas"]
 let questions = [
     name = ["jak masz na imie", "jak sie nazywasz", ],
     color = ["podaj kolor", "jaki jest twoj ulubiony kolor", "podaj ulubiony kolor", "podaj najladniejszy kolor", "jaka jest twoja ulubiona brawa"],
@@ -13,8 +14,6 @@ let questions = [
     city = ["podaj miasto", "jakie jest twoje ulubione miasto", "jakie miasto najbardziej lubisz"],
     eat = ["podaj jedzenie", "jaka jest twoja ulubiona potrawa", "jakie jest twoje ulubione jedzenie"],
     animal = ["podaj zwierze", "jakie jest twoje ulubione zwierze", "jakie zwierzatko najbardziej lubisz",],
-    date = ["data", "ktory dzis jest", "jaki mamy dzien", "co dzis jest"],
-    time = ["godzina", "ktora jest godzina", "jaki jest czas"],
 ]
 let answers = [
     "Jestem Octi z Flying Octopus",
@@ -23,73 +22,64 @@ let answers = [
     "Najlepiej bawiłam się w Nowym Jorku",
     "Nie mogę powiedzieć nic innego niż pierogi",
     "Popatrz na mnie, czy ty sobie jaja robisz?",
-    week[data.getDay()] + data.getDate()+"."+ (data.getUTCMonth()+1)+"."+ data.getFullYear(),
-    "Jest " + data.toLocaleString().slice(-8, -3)+" czasu polskiego",
 ]
 
-let week = ["Niedziela ",
-            "Poniedziałek ",
-            "Wtorek ",
-            "Środa ",
-            "Czwartek ",
-            "Piątek ",
-            "Sobota ",
-        ]
+function count(txt, m){
 
-function count(txt){
-
-    if(txt.includes("sqrt")){
-        t = txt.indexOf("sqrt") + 4
-        x = parseFloat(txt.slice(t)) 
-        if(x>=0 && isFinite(x))
-        return "Wynik działania "+ txt +" to "+ Math.sqrt(x).toFixed(3)
-        else
-        return "Błędne dane"
-    }
-    else if(txt.includes("+")){
-        t = txt.indexOf("+")
-        x = parseFloat(txt.slice(0,t)) 
-        y = parseFloat(txt.slice(t+1))
-        if(isFinite(x) && isFinite(y))
-        return "Wynik działania "+ txt +" to "+(x+y)
-        else
-        return "Błędne dane"
-    }
-    else if(txt.includes("-")){
-        t = txt.indexOf("-")
-        x = parseFloat(txt.slice(0,t)) 
-        y = parseFloat(txt.slice(t+1))
-        if(isFinite(x) && isFinite(y))
-        return "Wynik działania "+ txt +" to "+(x-y)
-        else
-        return "Błędne dane"
-    }
-    else if(txt.indexOf("*")>=0){
-        t = txt.indexOf("*")
-        x = parseFloat(txt.slice(0,t)) 
-        y = parseFloat(txt.slice(t+1))
-        if(isFinite(x) && isFinite(y))
-        return "Wynik działania "+ txt +" to "+(x*y)
-        else
-        return "Błędne dane"
-    }
-    else if(txt.indexOf("/")>=0){
-        t = txt.indexOf("/")
-        x = parseFloat(txt.slice(0,t)) 
-        y = parseFloat(txt.slice(t+1))
-        if(isFinite(x) && isFinite(y) && y!=0)
-        return "Wynik działania "+ txt +" to "+(x/y)
-        else
-        return "Błędne dane"
-    }
-    else{
-        t = txt.indexOf("%")
-        x = parseFloat(txt.slice(0,t)) 
-        y = parseFloat(txt.slice(t+1))
-        if(isFinite(x) && isFinite(y) && y!=0)
-        return "Wynik działania "+ txt +" to "+(x%y)
-        else
-        return "Błędne dane"
+    switch(true){
+        case m == 0:
+            t = txt.indexOf("+")
+            x = parseFloat(txt.slice(0,t)) 
+            y = parseFloat(txt.slice(t+1))
+            if(isFinite(x) && isFinite(y))
+                return "Wynik działania "+ txt +" to "+(x+y)
+            else
+                return "Błędne dane"
+            break
+        case m==1:
+            t = txt.indexOf("-")
+            x = parseFloat(txt.slice(0,t)) 
+            y = parseFloat(txt.slice(t+1))
+            if(isFinite(x) && isFinite(y))
+                return "Wynik działania "+ txt +" to "+(x-y)
+            else
+                return "Błędne dane"
+            break
+        case m==2:
+            t = txt.indexOf("*")
+            x = parseFloat(txt.slice(0,t)) 
+            y = parseFloat(txt.slice(t+1))
+            if(isFinite(x) && isFinite(y))
+                return "Wynik działania "+ txt +" to "+(x*y)
+            else
+                return "Błędne dane"
+            break
+        case m==3:
+            t = txt.indexOf("/")
+            x = parseFloat(txt.slice(0,t)) 
+            y = parseFloat(txt.slice(t+1))
+            if(isFinite(x) && isFinite(y) && y!=0)
+                return "Wynik działania "+ txt +" to "+(x/y)
+            else
+                return "Błędne dane"
+            break
+        case m==4:
+            t = txt.indexOf("%")
+            x = parseFloat(txt.slice(0,t)) 
+            y = parseFloat(txt.slice(t+1))
+            if(isFinite(x) && isFinite(y) && y!=0)
+                return "Wynik działania "+ txt +" to "+(x%y)
+            else
+                return "Błędne dane"
+            break
+        case m==5:
+            t = txt.indexOf("sqrt") + 4
+            x = parseFloat(txt.slice(t)) 
+            if(x>=0 && isFinite(x))
+                return "Pierwiastek z "+ x +" wynosi "+ Math.sqrt(x).toFixed(3)
+            else
+                return "Błędne dane"
+            break
     }
 }
 
@@ -101,7 +91,6 @@ function game(txt){
     let rock = ["kamień", "kamien", "Kamień", "Kamien"]
     let paper = ["Papier", "papier"]
     let scissors = ["Nożyce", "Nozyce", "nozyce", "nożyce", "nozyczki", "nożyczki", "Nozyczki", "Nożyczki"]
-
 
     if(txt.includes("stop") || txt.includes("koniec")){
         if(playerpkt>comppkt)
@@ -121,7 +110,7 @@ function game(txt){
         if(rock.includes(txt))
             txt="Kamień"
         else if(paper.includes(txt))
-            txt="Kamień"
+            txt="Papier"
         else   
             txt="Nożyce"
 
@@ -138,33 +127,34 @@ function game(txt){
             comp="Papier"
         else
             comp="Nożyce"
-        
-        if(txt==comp){
-            ans.innerHTML = comp+" - Remis"
-        }
-        else if(txt=="Kamień" && comp=="Nożyce"){
-            ans.innerHTML = comp+" - Punkt dla ciebie"
-            playerpkt++
-        }    
-        else if(txt=="Kamień" && comp=="Papier"){
-            ans.innerHTML = comp+" - Punkt dla mnie"
-            comppkt++
-        } 
-        else if(txt=="Papier" && comp=="Nożyce"){
-            ans.innerHTML = comp+" - Punkt dla mnie"
-            comppkt++
-        }    
-        else if(txt=="Papier" && comp=="Kamień"){
-            ans.innerHTML = comp+" - Punkt dla ciebie"
-            playerpkt++
-        } 
-        else if(txt=="Nożyce" && comp=="Papier"){
-            ans.innerHTML = comp+" - Punkt dla ciebie"
-            playerpkt++
-        }    
-        else if(txt=="Nożyce" && comp=="Kamień"){
-            ans.innerHTML = comp+" - Punkt dla mnie"
-            comppkt++
+        switch(true){
+            case (txt==comp):
+                ans.innerHTML = comp+" - Remis"
+                break
+            case (txt=="Kamień" && comp=="Nożyce"):
+                ans.innerHTML = comp+" - Punkt dla ciebie"
+                playerpkt++
+                break    
+            case (txt=="Kamień" && comp=="Papier"):
+                ans.innerHTML = comp+" - Punkt dla mnie"
+                comppkt++
+                break
+            case(txt=="Papier" && comp=="Nożyce"):
+                ans.innerHTML = comp+" - Punkt dla mnie"
+                comppkt++
+                break
+            case(txt=="Papier" && comp=="Kamień"):
+                ans.innerHTML = comp+" - Punkt dla ciebie"
+                playerpkt++
+                break 
+            case(txt=="Nożyce" && comp=="Papier"):
+                ans.innerHTML = comp+" - Punkt dla ciebie"
+                playerpkt++
+                break
+            case(txt=="Nożyce" && comp=="Kamień"):
+                ans.innerHTML = comp+" - Punkt dla mnie"
+                comppkt++
+                break
         } 
         score.innerHTML = "Ty "+playerpkt+" - "+comppkt+" Octi"
     }
@@ -173,30 +163,40 @@ function game(txt){
 }
 
 function answer (txt){
-    //proste pytania + czas
     let n = -1
+    let m = -1
     for(i=0; i<questions.length; i++){
         for(j=0; j<10; j++){
             if(txt.includes(questions[i][j]))
                 n = i
         }
     }
-    if(n>=0){
-    ans.innerHTML = answers[n]
-    }//funkcje matematyczne
-    else if(txt.includes("+") || txt.includes("-") || txt.includes("*") || txt.includes("/") || txt.includes("%") || txt.includes("sqrt")){
-        ans.innerHTML = count(txt)
-    }//kamien papier nozyce
-    else if(game_q.includes(txt)){
-        ans.innerHTML = "Dobrze zagrajmy - jak bedziesz gotowy wpisz Kamień, Papier lub Nożyce"
-        score.innerHTML = "Ty "+playerpkt+" - "+comppkt+" Octi"
-        g = true
-    }//else
-    else if(txt==""){
-        ans.innerHTML = "Napisz coś"
+    for(i=0; i<math_q.length; i++){
+        if(txt.includes(math_q[i]))
+            m = i
     }
-    else{
-        ans.innerHTML = "Nie rozumiem"
+    switch(true){
+        case n>=0:
+            ans.innerHTML = answers[n]
+            break
+        case m>=0:
+            ans.innerHTML = count(txt, m)
+            break
+        case date.includes(txt):
+            let data = new Date()
+            ans.innerHTML = week[data.getDay()] + data.getDate()+"."+ (data.getUTCMonth()+1)+"."+ data.getFullYear()
+            break
+        case time.includes(txt):
+            let godzina = new Date()
+            ans.innerHTML = "Jest " + godzina.toLocaleString().slice(-8, -3)+" czasu polskiego"
+            break
+        case game_q.includes(txt):
+            ans.innerHTML = "Dobrze zagrajmy - jak bedziesz gotowy wpisz Kamień, Papier lub Nożyce"
+            score.innerHTML = "Ty "+playerpkt+" - "+comppkt+" Octi"
+            g = true
+            break
+        default:
+            ans.innerHTML = "Nie rozumiem"
     }
 }
 
